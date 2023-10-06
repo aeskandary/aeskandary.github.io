8703fe5485b8bc72c02db28efc87024cafee3e33
Ali Eskandary <aeskandary77@gmail.com>
2023-10-06 12:24:41 -0400

    long awaited update
    adding photo section, pulsing arrow button, optimization on the images

---
+++ js/aeScript.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //open/close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  //add click events on buttons to open
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  //add click events to close
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+});
